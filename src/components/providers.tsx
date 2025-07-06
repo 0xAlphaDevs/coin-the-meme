932feb22b0b1ad8da32413fc251ac23f932364e6
0xyshv <writetoyshv@gmail.com>
2025-07-06 12:05:08 +0530

    fix: wallet connection

--- src/components/providers.tsx
+++ src/components/providers.tsx
@@ -1,13 +1,33 @@
-'use client'
+"use client";
 
-import { WagmiProvider } from 'wagmi'
-import { ConnectKitProvider } from 'connectkit'
-import { wagmiConfig } from '@/lib/wagmi'
+import { WagmiProvider, createConfig, http } from "wagmi";
+import { baseSepolia } from "wagmi/chains";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ConnectKitProvider, getDefaultConfig } from "connectkit";
 
-export function Providers({ children }: { children: React.ReactNode }) {
+const config = createConfig(
+  getDefaultConfig({
+    chains: [baseSepolia],
+    transports: {
+      [baseSepolia.id]: http(`https://base-sepolia.g.alchemy.com/v2/${process.env.NEXT_PUBLIC_ALCHEMY_ID}`)
+    },
+
+    walletConnectProjectId: process.env.PROJECT_ID as string,
+    appName: "Coin the Meme",
+    appDescription: "Your App Description",
+  }),
+);
+
+const queryClient = new QueryClient();
+
+import { ReactNode } from "react";
+
+export const Web3Provider = ({ children }: { children: ReactNode }) => {
   return (
-    <WagmiProvider config={wagmiConfig}>
-      <ConnectKitProvider>{children}</ConnectKitProvider>
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}>
+        <ConnectKitProvider>{children}</ConnectKitProvider>
+      </QueryClientProvider>
     </WagmiProvider>
-  )
-}
+  );
+};
